25e8f14fd1e9dffb690245f72aebe2f73849983e
Tank Prashad Awasthi <tankawasthi@cloudtechservice.com>
2025-08-18 18:32:54 +0545

    Revert "Add files from week 5 project"
    This reverts commit 81fcea3f925722f396a461856cabd4f37ee6d4bc.

--- revision/api_sync_backend/src/middlewares/requestLogger.js
+++
@@ -1,3 +0,0 @@
-const { morganMiddleware } = require('../config/logger');
-
-module.exports = morganMiddleware;
--- revision/api_sync_backend/src/routes/healthRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ status: 'OK', timestamp: new Date() });
-});
-
-module.exports = router;
